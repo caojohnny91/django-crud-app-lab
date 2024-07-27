845c75d31b1667ee2335532055f19e048f925cdf
jcao91 <jcao91@yahoo.com>
2024-07-26 21:08:34 -0500

    create author index page

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,6 +4,6 @@ from . import views  # Import views to connect routes to view functions
 urlpatterns = [
     # add routes below
     path("", views.home, name="home"),
-    path("index/", views.index, name="index"),
+    path("authors/", views.author_index, name="author-index"),
     path("detail/", views.detail, name="detail"),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -4,13 +4,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+class Author:
+    def __init__(self, name, date_of_birth, nationality):
+        self.name = name
+        self.date_of_birth = date_of_birth
+        self.nationality = nationality
+
+
+authors = [
+    Author("William Shakespeare", "April 23, 1564", "English"),
+    Author("Jane Austen", "December 16, 1775", "English"),
+    Author("J.K. Rowling", "July 31, 1965", "British"),
+    Author("George Orwell", "June 25, 1903", "British"),
+]
+
+
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
 
-def index(request):
-    return HttpResponse("<h1>Index BookCollector</h1>")
+def author_index(request):
+    return render(request, 'authors/index.html', {'authors': authors})
 
 
 def detail(request):
